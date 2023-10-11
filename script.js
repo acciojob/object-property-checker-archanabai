5bd01dd5a5f5cfc1430e7dfa841a55a48e851a9b
archanabai <121252186+archanabai@users.noreply.github.com>
2023-10-11 15:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+   let arr = Object.keys(sampleObject);
+
+	for(let i=0 ;i<=arr.length ;i++){
+		if (arr[i] == key){
+			return true;
+		}
+		
+	}return false;
+	
 }
 
 // Do not change the code below
